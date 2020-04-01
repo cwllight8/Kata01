528b97e5df0fe88c2af7b54fe374b3fc928bc429
cwllight8 <cwllight8@gmail.com>
2020-04-01 17:09:23 -0600

    added in the code i will submit with all tests compleated. it is at the bottom of the file contained within the PhoneDir class along witht the person type

--- Kata01/Algorithim.cs
+++ Kata01/Algorithim.cs
@@ -31,6 +31,7 @@ namespace Kata01
 
         public void seperate(string s)
         {
+            people.Clear();
             List<string> st = s.Trim().Split('\n').ToList();
             string phonePattern = @"\d*[-]\d{3}[-]\d{3}[-]\d{4}";
             Regex phoneRgx = new Regex(phonePattern);
@@ -40,6 +41,8 @@ namespace Kata01
 
             string addressPattern = @"([^a-z A-z.,0-9])";
             Regex addressRgx = new Regex(addressPattern);
+            string extraPattern = @"\s{2,}";
+            Regex extraSpaceRgx = new Regex(extraPattern);
 
             for (int i = 0; i < st.Count; i++)
             {
@@ -58,11 +61,12 @@ namespace Kata01
                 else { break; }
                 if (nameRgx.IsMatch(st[i]))
                 {
-                    name = addressRgx.Replace(nameM.Value, "");
+                    name = nameM.Value.Replace("<", "").Replace(">", "");
                     st[i] = nameRgx.Replace(st[i], "");
                 }
                 else { break; }
                 st[i] = addressRgx.Replace(st[i], "");
+                st[i] = extraSpaceRgx.Replace(st[i], " ");
                 address = st[i].Replace("_", " ");
 
                 people.Add(new Person(name, phone, address));
@@ -99,4 +103,103 @@ namespace Kata01
         }
 
     }
+
+    public class PhoneDir
+    {
+
+        public static List<Person> people = new List<Person>();
+
+        public static void seperate(string s)
+        {
+            people.Clear();
+
+            List<string> st = s.Trim().Split('\n').ToList();
+            string phonePattern = @"\d*[-]\d{3}[-]\d{3}[-]\d{4}";
+            Regex phoneRgx = new Regex(phonePattern);
+
+            string namePattern = @"<.*>";
+            Regex nameRgx = new Regex(namePattern);
+
+            string addressPattern = @"([^a-z A-z.,\-0-9])";
+            Regex addressRgx = new Regex(addressPattern);
+
+            string extraPattern = @"\s{2,}";
+            Regex extraSpaceRgx = new Regex(extraPattern);
+
+            for (int i = 0; i < st.Count; i++)
+            {
+                string phone = "";
+                string name = "";
+                string address = "";
+
+                Match phoneM = phoneRgx.Match(st[i]);
+                Match nameM = nameRgx.Match(st[i]);
+
+                if (phoneRgx.IsMatch(st[i]))
+                {
+                    phone = phoneM.Value;
+                    st[i] = phoneRgx.Replace(st[i], "");
+                }
+                else { break; }
+                if (nameRgx.IsMatch(st[i]))
+                {
+                    name = nameM.Value.Replace("<", "").Replace(">", "");
+                    st[i] = nameRgx.Replace(st[i], "");
+                }
+                else { break; }
+                st[i] = addressRgx.Replace(st[i], "");
+                st[i] = extraSpaceRgx.Replace(st[i], " ");
+                address = st[i].Replace("_", " ");
+
+                people.Add(new Person(name.Trim(), phone.Trim(), address.Trim()));
+            }
+        }
+
+        public static string Phone(string strng, string num)
+        {
+            seperate(strng);
+
+            int count = 0;
+            Person result = null;
+            foreach (Person p in people)
+            {
+                if (p.Num.Equals(num))
+                {
+                    count++;
+                    if (count > 1)
+                    {
+                        return "Error => Too many people: " + num;
+                    }
+                    result = p;
+                }
+            }
+            if (result == null)
+            {
+                return "Error => Not found: " + num;
+            }
+            else
+            {
+                return result.ToString();
+            }
+        }
+    }
+    //public class Person
+    //{
+    //    public string Name { get; set; }
+    //    public string Num { get; set; }
+    //    public string Address { get; set; }
+
+    //    public Person(string name, string num, string address)
+    //    {
+    //        Name = name;
+    //        Num = num;
+    //        Address = address;
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        return "Phone => " + Num + ", Name => " + Name + ", Address => " + Address;
+    //    }
+    //}
 }
+
--- Kata01/Program.cs
+++ Kata01/Program.cs
@@ -6,7 +6,7 @@ namespace Kata01
     {
         static void Main(string[] args)
         {
-            string s = "/+1-541-754-3010 156 Alphand_St. <J Steeve>\n 133, Green, Rd. <E Kustur> NY-56423 ;+1-541-914-3010!\n";
+            string s = "/+1-541-754-3010 156  Alphand_St. <J Steeve>\n 133, Green, Rd. <E Kustur> NY-56423 ;+1-541-914-3010!\n";
             Algorithim a = new Algorithim();
             
             string st = a.phone(s, "1-541-754-3010");
